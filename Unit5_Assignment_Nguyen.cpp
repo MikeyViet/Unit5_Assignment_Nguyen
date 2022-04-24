8b81f42fe35846ba692fc2dbb4e53d0a9c85fc6e
MikeyViet <vietwiin@gmail.com>
2022-04-24 09:53:10 -0500

    Configured VsCode with GitHub Repository. Configured launch.json and tasks.json to enable C++ compiler.  Started main() function.

---
+++ Unit5_Assignment_Nguyen.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************************
+ *  Unit 5 Assignment - Vectors, Functions, Conditions, Loops, Color, and
+ *      Sorting Values
+ * 
+ *  Student:    Michael Nguyen
+ *  Class:       COSC-1437 Spring 2022
+ *  Teacher:   Dr. Tyson McMillan
+ *  
+ *  Description:    This program will demonstrate Accepting numbers into 
+ *  a vector, getting the average, using bubble sort (or another algorithm 
+ *  native to vector sorting methods) to sort the values from Greatest to 
+ *  Least, Re-write to a file, upgrading from Struct to an Object-Oriented 
+ *  menu process.
+***********************************************************************************/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    cout << "VsCodeWorking now with GitHub" << endl;
+
+    return 0;
+}
+
